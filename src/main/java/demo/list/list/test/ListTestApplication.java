9787d40d92f629dab1acf7f2e5d82fae8cdb1bf4
peroff <peroff007@gmail.com>
2024-07-30 03:22:32 +0300

    initial commit

---
+++ src/main/java/demo/list/list/test/ListTestApplication.java
@@ -0,0 +1,13 @@
+package demo.list.list.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/list/list/test/ListTestApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.list.list.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
